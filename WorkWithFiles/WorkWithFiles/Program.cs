9dc31eb9f30c7593e71a8f9fb56eb8614e550e4a
Richard <94477473+kori43@users.noreply.github.com>
2025-01-03 20:50:29 +0500

    add WorkWithFiles

---
+++ WorkWithFiles/WorkWithFiles/Program.cs
@@ -0,0 +1,44 @@
+﻿string filePath = "data.txt";
+
+//--------------------------------------------------------------------------------
+
+//string content = "Hello, World!";
+
+////Запись в файл
+//File.WriteAllText(filePath, content);
+
+//Console.WriteLine($"файл '{filePath}' успешно создан и записан.");
+
+//--------------------------------------------------------------------------------
+
+////Проверка существования файла
+//if (File.Exists(filePath))
+//{
+//    string content = File.ReadAllText(filePath);
+//    Console.WriteLine($"содержимое файла '{filePath}':");
+//    Console.WriteLine(content);
+//}
+//else
+//    Console.WriteLine($"файл '{filePath}' не найден.");
+
+//--------------------------------------------------------------------------------
+
+//string newContent = "Hello, everyone!";
+
+//// Добавление текста в файл
+//File.AppendAllText(filePath, newContent + Environment.NewLine);
+
+//Console.WriteLine($"Новая строка успешно добавлена в файл '{filePath}'.");
+
+//--------------------------------------------------------------------------------
+
+//// Удаление файла
+//if(File.Exists(filePath))
+//{
+//    File.Delete(filePath);
+//    Console.WriteLine($"Файл '{filePath}' успешно удален.");
+//}
+//else
+//{
+//    Console.WriteLine($"файл '{filePath}' не существует.");
+//}
